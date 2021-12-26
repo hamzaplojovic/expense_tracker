f8cfaae0ff09422f7bace88ceeb181e844679346
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-12-26 18:29:52 +0100

    First app?

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>My first app</h1>
+        </div>
+    );
+}
+
+export default App;

8330cc1e373a10ccb074d988e93be2271d7cedca
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-01-03 19:10:31 +0100

    Expense Item

--- app/src/components/ExpenseItem.js
+++ app/src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
 // A component in React is a function!!!
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>;
+    return (
+        <div className="expense-item">
+            <div>January 12th 2022</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$293.4</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;

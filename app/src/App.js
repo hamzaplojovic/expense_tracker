e1b32f29ec2fc8d718d7082e1101c653ac5a3bd9
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-01-03 16:15:55 +0100

    new app

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,9 @@
-import Todo from "./components/Todo";
-
 function App() {
-    return (
-        <div>
-            <h1>My Todos</h1>
-            <Todo text="Learn React" />
-        </div>
-    );
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
 }
 
 export default App;

0ac1b5f95110655b8d1a37fead53c8a72579865d
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-01-07 15:59:00 +0100

    Adding event listeners

--- app/src/components/ExpenseItem.js
+++ app/src/components/ExpenseItem.js
@@ -1,7 +1,11 @@
 // A component in React is a function!!!
 import "./ExpenseItem.css";
+import swal from "sweetalert";
 
 function ExpenseItem(props) {
+    const clickHandler = () => {
+        swal("Hello world!");
+    };
     return (
         <div className="expense-item">
             <div id="date">{props.date}</div>
@@ -9,6 +13,7 @@ function ExpenseItem(props) {
                 <h2>{props.name}</h2>
                 <div className="expense-item__price">{"$" + props.price}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </div>
     );
 }

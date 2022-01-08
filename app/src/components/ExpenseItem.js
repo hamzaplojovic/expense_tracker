33029a8f96acaa31d16953b83e7d93ea03262e60
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-01-08 12:32:39 +0100

    useState

--- app/src/components/ExpenseItem.js
+++ app/src/components/ExpenseItem.js
@@ -1,16 +1,18 @@
 // A component in React is a function!!!
 import "./ExpenseItem.css";
-import swal from "sweetalert";
+import React, { useState } from "react";
 
 function ExpenseItem(props) {
+    const [name, setName] = useState(props.name);
     const clickHandler = () => {
-        swal("Hello world!");
+        setName("Updated");
     };
+
     return (
         <div className="expense-item">
             <div id="date">{props.date}</div>
             <div className="expense-item__description">
-                <h2>{props.name}</h2>
+                <h2>{name}</h2>
                 <div className="expense-item__price">{"$" + props.price}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>

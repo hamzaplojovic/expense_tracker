7815887fdbc824e92cb68fbd02eca11f676014d6
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-01-09 13:52:48 +0100

    Debugging

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,42 @@
-import ExpenseItem from "./components/Expenses/ExpenseItem";
+import React from "react";
 
-function App() {
+import NewExpense from "./components/New Expense/NewExpense";
+import Expenses from "./components/Expenses/ExpenseItem";
+
+const App = () => {
+    const expenses = [
+        {
+            id: "e1",
+            title: "Toilet Paper",
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        {
+            id: "e2",
+            title: "New TV",
+            amount: 799.49,
+            date: new Date(2021, 2, 12),
+        },
+        {
+            id: "e3",
+            title: "Car Insurance",
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: "e4",
+            title: "New Desk (Wooden)",
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
     return (
         <div>
-            <ExpenseItem
-                name="Car Insurance"
-                price="293.4"
-                date="12th January 2022"
-            />
-            <ExpenseItem
-                name="Internet Costs"
-                price="15"
-                date="31th November 2022"
-            />
+            <NewExpense />
+            <Expenses items={expenses} />
         </div>
     );
-}
+};
 
 export default App;

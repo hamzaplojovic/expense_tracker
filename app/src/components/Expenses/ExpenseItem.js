37010afc5514d0f42174efd7e2a06491dacec905
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-01-09 13:59:11 +0100

    Backup

--- app/src/components/Expenses/ExpenseItem.js
+++ app/src/components/Expenses/ExpenseItem.js
@@ -3,19 +3,13 @@ import "./ExpenseItem.css";
 import React, { useState } from "react";
 
 function ExpenseItem(props) {
-    const [name, setName] = useState(props.name);
-    const clickHandler = () => {
-        setName("Updated");
-    };
-
     return (
         <div className="expense-item">
             <div id="date">{props.date}</div>
             <div className="expense-item__description">
-                <h2>{name}</h2>
+                <h2>{props.name}</h2>
                 <div className="expense-item__price">{"$" + props.price}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </div>
     );
 }
